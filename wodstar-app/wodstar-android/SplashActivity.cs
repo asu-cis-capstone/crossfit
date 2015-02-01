8be94f0fd5b30c050493cb0f04a4501d3583efca
Steven Beverly <steven.beverly@asu.edu>
2015-01-31 18:37:46 -0700

    Proof of concept for Android. Includes a splash screen and login page.

---
+++ wodstar-app/wodstar-android/SplashActivity.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace wodstarandroid
+{
+	[Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+	public class SplashActivity : Activity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+			Thread.Sleep(5000); // Simulate a long loading process on app startup.
+			StartActivity(typeof(MainActivity));
+		}
+	}
+}
+
