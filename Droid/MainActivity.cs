e6240638c338edae5690bbc708660378da2c719a
Steven Beverly <steven.beverly@asu.edu>
2015-02-07 16:41:47 -0700

    Core: No change Android: application name, application icon, splash screen IOS: No change

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace WodstarMobileApp.Droid
 {
-	[Activity (Label = "WodstarMobileApp.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "WodstarMobileApp.Droid", Icon = "@drawable/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
---
+++ Droid/SplashActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+//Create splash screen activity as the MainLauncher, then call MainActivity
+namespace WodstarMobileApp.Droid
+{
+	[Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true, Label = "Wodstar", Icon = "@drawable/icon")]
+	public class SplashActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			//start the main activity
+			StartActivity(typeof(MainActivity));
+		}
+	}
+}
+
