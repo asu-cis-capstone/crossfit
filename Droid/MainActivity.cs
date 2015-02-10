f5cbd9591e7ae1dcc90cfb71f6216a84045c5682
Steven Beverly <steven.beverly@asu.edu>
2015-02-09 18:02:51 -0700

    Merging!

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 
 using Android.App;
@@ -12,6 +11,8 @@ using Android.OS;
 using Xamarin.Facebook;
 using Xamarin.Facebook.Model;
 using Xamarin.Facebook.Widget;
+using Microsoft.WindowsAzure.MobileServices;
+
 
 [assembly:Permission (Name = Android.Manifest.Permission.Internet)]
 [assembly:Permission (Name = Android.Manifest.Permission.WriteExternalStorage)]
@@ -30,49 +31,11 @@ namespace WodstarMobileApp.Droid
 			// Set our view from the "Login" layout resource
 			SetContentView (Resource.Layout.Login);
 		}
+
+		//public static MobileServiceClient MobileService = new MobileServiceClient (
+		//	"https://wodstar-helloworld.azure-mobile.net/",
+		//	"VESEBrXxDLeGQSOwHEqnNxtKmYyQDJ98"
+
+		//);
 	}
 }
-
-=======
-﻿using System;
-
-using Microsoft.WindowsAzure.MobileServices;
-using Android.App;
-using Android.Content;
-using Android.Content.PM;
-using Android.Runtime;
-//using Android.Support.V4.App;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
-//using Xamarin.Facebook;
-//using Xamarin.Facebook.Model;
-//using Xamarin.Facebook.Widget;
-
-[assembly:Permission (Name = Android.Manifest.Permission.Internet)]
-[assembly:Permission (Name = Android.Manifest.Permission.WriteExternalStorage)]
-[assembly:MetaData ("com.facebook.sdk.ApplicationId", Value ="@string/facebook_app_id")]
-
-
-namespace WodstarMobileApp.Droid
-{
-	[Activity (Label = "WodstarMobileApp.Droid", Icon = "@drawable/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : Activity
-	{
-		public static MobileServiceClient MobileService = new MobileServiceClient (
-			"https://wodstar-helloworld.azure-mobile.net/",
-			"VESEBrXxDLeGQSOwHEqnNxtKmYyQDJ98"
-
-      	);
-			
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
-
-			// Set our view from the "Login" layout resource
-			SetContentView (Resource.Layout.Login);
-		}
-	}
-}
-
->>>>>>> 2743bc8bee727247dfa79fc69c6c4d7d48aa1cc4
