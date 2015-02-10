9c16d7d12f0e124e6c590d4c82477e1625028e85
Laura Gagliano <laura.k.gagliano@gmail.com>
2015-02-10 11:22:41 -0700

    Added more Activity class files

---
+++ Droid/MovementLibraryActivity.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace WodstarMobileApp.Droid
+{
+	[Activity (Label = "MovementLibraryActivity", Theme="@android:style/Theme.Black.NoTitleBar", Icon = "@drawable/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]			
+	public class MovementLibraryActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Create your application here
+			SetContentView (Resource.Layout.MovementLibrary);
+		}
+	}
+}
+
---
+++ Droid/UserProfileActivity.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace WodstarMobileApp.Droid
+{
+	[Activity (Label = "UserProfileActivity", Theme="@android:style/Theme.Black.NoTitleBar", Icon = "@drawable/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]			
+	public class UserProfileActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Create your application here
+			SetContentView (Resource.Layout.UserProfile);
+
+			//Code to go get Azure data - should be housed in non-OS specific code. 
+		}
+	}
+}
+
