5985cf1c4a6a02f40c60b66863f909077dee939d
Steven Beverly <steven.beverly@asu.edu>
2015-03-01 19:29:16 -0700

    - Add Azure Mobile Service Visual Studio 2013 project - This project contains the necessary code to interface between Azure Mobile Service and the Azure Database; is the middleware between mobile applications and the Azure Database service

---
+++ wodstar_AzureMS/wodstarService/App_Start/WebApiConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Mobile.Service;
+using wodstarService.DataObjects;
+using wodstarService.Models;
+
+namespace wodstarService
+{
+    public static class WebApiConfig
+    {
+        public static void Register()
+        {
+            // Use this class to set configuration options for your mobile service
+            ConfigOptions options = new ConfigOptions();
+
+            // Use this class to set WebAPI configuration options
+            HttpConfiguration config = ServiceConfig.Initialize(new ConfigBuilder(options));
+
+            // To display errors in the browser during development, uncomment the following
+            // line. Comment it out again when you deploy your service for production use.
+            // config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+            
+            //Database.SetInitializer(new wodstarInitializer());
+        }
+    }
+
+    //Disabled by sbeverly to avoid destroying the existing tables during Publishing to Azure Mobile Service
+    //public class wodstarInitializer : ClearDatabaseSchemaIfModelChanges<wodstarContext>
+    //{
+        //protected override void Seed(wodstarContext context)
+        //{
+            //List<UserAccount> userAccounts = new List<UserAccount>
+            //{
+                //new UserAccount { Id = Guid.NewGuid().ToString(), Username = "wodstar_admin", AccountType = "wodstar", FirstName = "Wodstar", LastName = "Admin" }
+            //};
+
+            //foreach (UserAccount userAccount in userAccounts)
+            //{
+                //context.Set<UserAccount>().Add(userAccount);
+            //}
+
+            //base.Seed(context);
+        //}
+    //}
+}
+
---
+++ wodstar_AzureMS/wodstarService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="wodstarService.WebApiApplication" Language="C#" %>
