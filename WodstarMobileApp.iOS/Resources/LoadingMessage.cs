e9bcc6afa9f5395987e08fc6fc3b3816d10fdc5e
krisser <kevinanthonyrisser@gmail.com>
2015-03-02 23:44:30 -0700

    iOS

---
+++ WodstarMobileApp.iOS/Resources/LoadingMessage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace WodstarMobileApp.iOS
+{
+	public class LoadingOverlay : UIView { //Taken from Xamarin recipe at http://developer.xamarin.com/recipes/ios/standard_controls/popovers/display_a_loading_message/
+		// control declarations
+		UIActivityIndicatorView activitySpinner;
+		UILabel loadingLabel;
+
+		public LoadingOverlay (RectangleF frame) : base (frame)
+		{
+			// configurable bits
+			BackgroundColor = UIColor.Black;
+			Alpha = 0.75f;
+			AutoresizingMask = UIViewAutoresizing.FlexibleDimensions;
+
+			nfloat labelHeight = 22;
+			nfloat labelWidth = Frame.Width - 20;
+
+			// derive the center x and y
+			nfloat centerX = Frame.Width / 2;
+			nfloat centerY = Frame.Height / 2;
+
+			// create the activity spinner, center it horizontall and put it 5 points above center x
+			activitySpinner = new UIActivityIndicatorView(UIActivityIndicatorViewStyle.WhiteLarge);
+			activitySpinner.Frame = new RectangleF (
+				centerX - (activitySpinner.Frame.Width / 2) ,
+				centerY - activitySpinner.Frame.Height - 20 ,
+				activitySpinner.Frame.Width ,
+				activitySpinner.Frame.Height);
+			activitySpinner.AutoresizingMask = UIViewAutoresizing.FlexibleMargins;
+			AddSubview (activitySpinner);
+			activitySpinner.StartAnimating ();
+
+			// create and configure the "Loading Data" label
+			loadingLabel = new UILabel(new RectangleF (
+				centerX - (labelWidth / 2),
+				centerY + 20 ,
+				labelWidth ,
+				labelHeight
+			));
+			loadingLabel.BackgroundColor = UIColor.Clear;
+			loadingLabel.TextColor = UIColor.White;
+			loadingLabel.Text = "Loading Data...";
+			loadingLabel.TextAlignment = UITextAlignment.Center;
+			loadingLabel.AutoresizingMask = UIViewAutoresizing.FlexibleMargins;
+			AddSubview (loadingLabel);
+		}
+
+		/// <summary>
+		/// Fades out the control and then removes it from the super view
+		/// </summary>
+		public void Hide ()
+		{
+			UIView.Animate (
+				0.5, // duration
+				() => { Alpha = 0; },
+				() => { RemoveFromSuperview(); }
+			);
+		}
+	};
+}
+
