d6623fde2d8daa0d21d4391dfcf2d99225281cdf
Steven Beverly <steven.beverly@asu.edu>
2015-03-17 10:06:59 -0700

    - add workoutId to link to the workout table

--- WodstarMobileApp/WorkoutSegment.cs
+++ WodstarMobileApp/WorkoutSegment.cs
@@ -5,15 +5,17 @@ namespace WodstarMobileApp
 	public class WorkoutSegment
 	{
 		public String segmentType;
+		public String workoutId;
 		public Movement[] segmentMovements;
 		public String[] movementDescriptions;
 		public String segmentHeader;
 		public String segmentDescription;
 		public int repetitions = 1;
 
-		public WorkoutSegment (String type, String header, String description, int repetitions, String[] movementDescriptions, params Movement[] movements)
+		public WorkoutSegment (String type, String workoutId, String header, String description, int repetitions, String[] movementDescriptions, params Movement[] movements)
 		{
 			this.segmentType = type;
+			this.workoutId = workoutId;
 			this.segmentHeader = header;
 			this.segmentMovements = movements;
 			this.repetitions = repetitions;
