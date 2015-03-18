09154cb1122bdf2be4cbc0224b401b022a5dff5a
krisser <kevinanthonyrisser@gmail.com>
2015-03-18 07:23:57 -0700

    iOS layout changes

---
+++ WodstarMobileApp.iOS/obj/Xcode/5/DerivedData/WodstarMobileApp.iOS/Build/Intermediates/WodstarMobileApp.iOS.build/MonoTouch-iphonesimulator/WodstarMobileApp.iOS.build/Objects-normal/i386/AppDelegate-9154F0790F19C4CA.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/krisser/asu-cis-capstone/crossfit/WodstarMobileApp.iOS/obj/Xcode/5/AppDelegate.m \
+  /Users/krisser/asu-cis-capstone/crossfit/WodstarMobileApp.iOS/obj/Xcode/5/AppDelegate.h
---
+++ WodstarMobileApp.iOS/obj/Xcode/5/DerivedData/WodstarMobileApp.iOS/Build/Intermediates/WodstarMobileApp.iOS.build/MonoTouch-iphonesimulator/WodstarMobileApp.iOS.build/Objects-normal/i386/DetailViewController-D4F52BE7AD24ECCB.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/krisser/asu-cis-capstone/crossfit/WodstarMobileApp.iOS/obj/Xcode/5/DetailViewController.m \
+  /Users/krisser/asu-cis-capstone/crossfit/WodstarMobileApp.iOS/obj/Xcode/5/DetailViewController.h
