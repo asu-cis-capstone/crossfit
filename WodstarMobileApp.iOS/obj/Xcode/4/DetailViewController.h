0fa5acaeb1478567f46058e5e228fe54b12699b8
Laura Gagliano <laura.k.gagliano@gmail.com>
2015-03-19 10:07:12 -0700

    Adding user profile dynamic loading

---
+++ WodstarMobileApp.iOS/obj/Xcode/4/DetailViewController.h
@@ -0,0 +1,21 @@
+// WARNING
+// This file has been generated automatically by Xamarin Studio to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface DetailViewController : UIViewController {
+	UILabel *_detailDescriptionLabel;
+	UIToolbar *_toolbar;
+}
+
+@property (nonatomic, retain) IBOutlet UILabel *detailDescriptionLabel;
+
+@property (nonatomic, retain) IBOutlet UIToolbar *toolbar;
+
+@end
