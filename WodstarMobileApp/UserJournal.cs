db058a44c62a5fb76939d539ca5c8243cf3c9c93
Steven Beverly <steven.beverly@asu.edu>
2015-03-19 21:27:31 -0700

    - Update UserJournal DOM for Azure

--- WodstarMobileApp/UserJournal.cs
+++ WodstarMobileApp/UserJournal.cs
@@ -1,13 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace WodstarMobileApp
 {
 	public class UserJournal
 	{
-		public int userID;
-		public List<List<int>> workoutStats;
-		public List<List<int>> prStats;
+		[JsonProperty(PropertyName = "id")]
+		public string id { get; set;}
+
+		[JsonProperty(PropertyName = "userAccountId")]
+		public string userAccountId;
+
+		[JsonProperty(PropertyName = "statType")]
+		public string statType { get; set; }
+
+		[JsonProperty(PropertyName = "statName")]
+		public string statName { get; set; }
+
+		[JsonProperty(PropertyName = "statResult")]
+		public string statResult { get; set; }
+
 
 		public UserJournal ()
 		{
